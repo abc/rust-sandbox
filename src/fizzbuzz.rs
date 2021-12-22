826c4543ecf41caf925869007bd0a8b4c6f0e6b7
Alastair Campbell <alastair.campbell@runbox.no>
2021-12-22 22:01:27 +0000

    Add basic fizzbuzz code.

---
+++ src/fizzbuzz.rs
@@ -0,0 +1,20 @@
+pub fn run() {
+    for n in 1..101 {
+        // If n is divisible by 3 and 5, print FizzBuzz.
+        if n % 3 == 0 && n % 5 == 0 {
+            println!("FizzBuzz");
+        }
+        // If n is divisible by 3, print Fizz.
+        else if n % 3 == 0 {
+            println!("Fizz");
+        }
+        // If n is divisible by 5, print Buzz.
+        else if n % 5 == 0 {
+            println!("Buzz");
+        }
+        // Otherwise, just print the number.
+        else {
+            println!("{}", n);
+        }
+    }
+}
